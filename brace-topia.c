1f5757dbc505bfec918862c7b7ea9cb1f8779813
mmk9233 <marinakania4@gmail.com>
2026-03-17 00:32:40 -0400

    Added the initialize grid function along with the stderr messages for the commands.

--- brace-topia.c
+++ brace-topia.c
@@ -7,7 +7,7 @@
 #include <getopt.h>
 #include <unistd.h>
 
-int mirco_delay = 900000;
+int micro_delay = 900000;
 //Negative count means unset or infinite mode
 int count = -1;
 int dimension = 15;
@@ -39,7 +39,43 @@ void print_usage() {
     "percent Endline braces. Others want Newline.");
 }
 
+void initialize_grid(int dim, int vacant, int endline){
+    char grid[dim][dim];
+    int total = dim * dim;
+    int vacant_amount = (vacant/100) * total;
+    int endline_amount = ((endline/100) * total) - vacant_amount;
+    int newline_amount = total - (endline_amount + vacant_amount);
+    char flat[total];
+    int index = 0;
+    for(int i = 0; i < vacant_amount; i++){
+        flat[index] = '.';
+        index++;
+    }
+    for(int j = 0; j < endline_amount; j++){
+        flat[index] = 'e';
+        index++;
+    }
+    for(int k =0; k < newline_amount; k++){
+        flat[index] = 'n';
+        index++;
+    }
+    //The Fisher-Yates Shuffle
+    for(int i = total -1; i > 0; i--){
+        int j = rand % (i+1);
+        char tmp = flat[i];
+        flat[i] = flat[j];
+        flat[j] = tmp;
+    }
 
+    //Copy into the 2-D array created above
+    index = 0
+    for(int r = 0; r < dim; r++){
+        for(int c = 0; c < dim; c++){
+            grid[r][c] = flat [index];
+            index++;
+        }
+    }
+}
 
 int main( int argc, char * argv[] ){
     while ( (opt = getopt( argc, argv, "ht:c:d:s:v:e:") ) != -1 ) {
@@ -54,13 +90,21 @@ int main( int argc, char * argv[] ){
                 int tmp_t = (int)strtol(optarg, NULL, 10);
                 if(tmp_t > 0){
                     micro_delay = tmp_t;
+                }else{
+                    fprintf(stderr, "Delay (%d) must be a positive integer", tmp_t);
+                    print_usage();
+                    return EXIT_FAILURE;
                 }
                 break;
             //Max count value
             case 'c':
                 int tmp_c = (int)strtol(optarg, NULL, 10);
-                if(tmp_c >= 0){
+                if(tmp_c > 0){
                     count = tmp_c;
+                }else{
+                    fprintf(stderr, "Count (%d) must be a positive integer", tmp_c);
+                    print_usage();
+                    return EXIT_FAILURE;
                 }
                 break;
             //Sets the dimensions
@@ -68,6 +112,11 @@ int main( int argc, char * argv[] ){
                 int tmp_d = (int)strtol(optarg, NULL, 10);
                 if(tmp_d >= 5 && tmp_d <= 39){
                     dimension = tmp_d;
+                }else{
+                    fprintf(stderr, "Dimension (%d) must be an integer [5..39]",
+                    tmp_d);
+                    print_usage();
+                    return EXIT_FAILURE;
                 }
                 break;
             //Sets the strength of preference
@@ -75,6 +124,11 @@ int main( int argc, char * argv[] ){
                 int tmp_s = (int)strtol(optarg, NULL, 10);
                 if(tmp_s > 0){
                     strength = tmp_s;
+                }else{
+                    fprintf(stderr, "Strength (%d) must be a positive integer", 
+                    tmp_s);
+                    print_usage();
+                    return EXIT_FAILURE;
                 }
                 break;
             //Sets the percent vacancies
@@ -82,6 +136,11 @@ int main( int argc, char * argv[] ){
                 int tmp_v = (int)strtol(optarg, NULL, 10);
                 if(tmp_v > 0){
                     vacancy = tmp_v;
+                }else{
+                    fprintf(stderr, "Vacancy (%d) must be a positive integer",
+                    tmp_v);
+                    print_usage();
+                    return EXIT_FAILURE;
                 }
                 break;
             //Sets the percent of endline braces
@@ -89,11 +148,31 @@ int main( int argc, char * argv[] ){
                 int tmp_e = (int)strtol(optarg, NULL, 10);
                 if(tmp_e > 0){
                     percent_endline = tmp_e;
+                }else{
+                    fprintf(stderr, "Endline Proportions (%d) must be a positive 
+                    integer", tmp_e);
+                    print_usage();
+                    return EXIT_FAILURE;
                 }
                 break;
             default:
+            fprintf(stderr, "brace-topia: invalid option -- '%c'", opt);
             print_usage();
             return EXIT_FAILURE;
+
+            //Randomize it
+            srandom(41);
+
+            //Initialize grid
+            initialize_grid(dimension, vacancy, percent_endline);
+
+            //Check for infinite mode
+            if(count != -1){
+            
+            }else{
+                usleep(micro_delay);        
+            }
+
         }
     }
 }
